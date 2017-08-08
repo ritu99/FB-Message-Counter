4f5bbf880ed820d749908254786861caeb475cab
Ritvik Annam <ritu6ritu@gmail.com>
2017-08-07 23:48:16 -0500

    Working message counter

--- index.js
+++ index.js
@@ -4,39 +4,64 @@ var fb = require("facebook-chat-api");
 var bodyParser = require("body-parser");
 var multer = require("multer");
 
-app.use(express.static('static'));
+app.use(express.static('views'));
+
 app.use(bodyParser.urlencoded({
     extended: true
 }));
 app.use(bodyParser.json());
 
+app.set('view engine', 'ejs');
+
 app.get('/', function (req, res) {
-   res.sendFile( __dirname + "/" + "index.html" );
+   res.render( __dirname + "/views/" + "login.ejs" );
 })
 
-function compare(a,b) {
-  return a.messageCount - b.messageCount;
-}
-
-app.post('/login',function (req, res) {
+app.post('/data',function (req, res) {
    // Prepare output in JSON format
    response = {
       username:req.body.username,
       password:req.body.password
    };
 
+   function compare(a,b) {
+     return b.messageCount - a.messageCount;
+   }
+
    console.log(response);
+   var threadCount = 1000;
    fb({email: response.username, password: response.password}, (err, api) => {
        if(err) return console.error(err);
        // Here you can use the api
-       api.getThreadList(0,300, "inbox", (err, arr) => {
+       api.getThreadList(0, threadCount, "inbox", (err, arr) => {
          if(err) return console.error(err);
          arr.sort(compare);
+         var count = 0;
+        //  console.log(arr);
+
+         var result = arr.map(a => a.threadID);
+         api.getUserInfo(result, (err, ret) => {
+             if(err) return console.error(err);
+             console.log(ret);
+             for(var i = 0; i < arr.length; i++){
+               if(arr[i]["participants"].length > 2) continue;
+               console.log('here');
+               if(typeof ret[arr[i]["threadID"]] !== "undefined"){
+                 arr[i]["name"] = ret[arr[i]["threadID"]]["name"];
+                 arr[i]["imageSrc"] = ret[arr[i]["threadID"]]["thumbSrc"];
+               }
+
+             }
+             console.log(arr);
+             res.render( __dirname + "/views/data.ejs", {data: arr});
+             api.logout();
+          });
+
        });
-       api.logout()
+
    });
 
-   res.end("Logged In - logging out now!");
+
 })
 
 var server = app.listen(8081, 'localhost', function () {
