a99e3444853c305e1a4d0f3022cbbc48c848b56b
Ritvik Annam <ritu6ritu@gmail.com>
2017-08-08 12:16:24 -0500

    Error Messages

--- index.js
+++ index.js
@@ -14,10 +14,10 @@ app.use(bodyParser.json());
 app.set('view engine', 'ejs');
 
 app.get('/', function (req, res) {
-   res.render( __dirname + "/views/" + "login.ejs" );
+   res.render( __dirname + "/views/" + "login.ejs", {data: 'nothing'} );
 })
 
-app.post('/data',function (req, res) {
+app.post('/',function (req, res) {
    // Prepare output in JSON format
    response = {
       username:req.body.username,
@@ -28,13 +28,25 @@ app.post('/data',function (req, res) {
      return b.messageCount - a.messageCount;
    }
 
-   console.log(response);
    var threadCount = 1000;
    fb({email: response.username, password: response.password}, (err, api) => {
-       if(err) return console.error(err);
+       if(err) {
+         switch(err.error){
+           case 'Wrong username/password.':
+              res.render( __dirname + "/views/" + "login.ejs", {data: "error"});
+              break;
+           case 'login-approval':
+              res.render( __dirname + "/views/" + "login.ejs", {data: "login-approval"});
+              break;
+         }
+
+         return console.error(err);
+       }
        // Here you can use the api
        api.getThreadList(0, threadCount, "inbox", (err, arr) => {
-         if(err) return console.error(err);
+         if(err) {
+           return console.error(err);
+         }
          arr.sort(compare);
          var count = 0;
         //  console.log(arr);
@@ -42,17 +54,14 @@ app.post('/data',function (req, res) {
          var result = arr.map(a => a.threadID);
          api.getUserInfo(result, (err, ret) => {
              if(err) return console.error(err);
-             console.log(ret);
              for(var i = 0; i < arr.length; i++){
                if(arr[i]["participants"].length > 2) continue;
-               console.log('here');
                if(typeof ret[arr[i]["threadID"]] !== "undefined"){
                  arr[i]["name"] = ret[arr[i]["threadID"]]["name"];
                  arr[i]["imageSrc"] = ret[arr[i]["threadID"]]["thumbSrc"];
                }
 
              }
-             console.log(arr);
              res.render( __dirname + "/views/data.ejs", {data: arr});
              api.logout();
           });
